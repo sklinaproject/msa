34398e592c420d7a677d4bde28136e35366c9727
kyungje <kyungje@gmail.com>
2021-05-10 17:37:40 +0900

    init

---
+++ src/test/java/sk/lina/msa/MsaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.lina.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
